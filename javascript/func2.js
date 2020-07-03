f7add372b2940c2d75ac81871c2a54d2c288b716
unknown <tnehdtlrl@naver.com>
2020-07-03 18:05:15 +0900

    0703

---
+++ javascript/func2.js
@@ -0,0 +1,34 @@
+
+function fun1() {
+    // document.getElementById("show").innerHTML = "Hello";   
+}
+fun1();
+
+var fun2 = function(str) {
+    document.getElementById("show").innerHTML = str;
+}
+// fun2("Hello");
+
+function outerFunc(func) {
+    func(2,3,4,5,6);
+}
+
+function argsFunc() {
+    var sum = 0;
+    for(var i=0; i<arguments.length; i++) {
+        sum += arguments[i];
+    }
+    document.getElementById("show").innerHTML = sum;
+}
+
+outerFunc(argsFunc);
+/* 
+↑ = ↓
+outerFunc(function argsFunc(2,3,4,5,6)) {
+      var sum = 0;
+    for(var i=0; i<arguments.length; i++) {
+        sum += arguments[i];
+    }
+    document.getElementById("show").innerHTML = sum;
+}) ();
+*/
